9da024fa0c9691a8be3a8ad7be1a244742aad42d
Marat Field <mrpolyansky@yandex.ru>
2023-01-15 18:10:21 +0300

    Added methods and verification of meeting date confirmation

--- src/test/java/ru/netology/selenide/DeliveryCardsTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardsTest.java
@@ -1,6 +1,7 @@
 package ru.netology.selenide;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,7 +15,6 @@ import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardsTest {
-    private LocalDate date = LocalDate.now();
 
     @BeforeEach
     void setUp() {
@@ -23,40 +23,45 @@ public class DeliveryCardsTest {
 
     @Test
     void shouldTestDeliveryCard() {
-        LocalDate dateNew = date.plusDays(4);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String formattedDateTime = dateNew.format(formatter);
+        long daysGap = 4;
+        String newDatePattern = "dd.MM.yyyy";
 
         $x("//*[@data-test-id='city']//input").setValue("Краснодар");
         $x("//*[@data-test-id='date']//input").sendKeys(Keys.CONTROL + "a");
         $x("//*[@data-test-id='date']//input").sendKeys(Keys.BACK_SPACE);
-        $x("//*[@data-test-id='date']//input").sendKeys(formattedDateTime);
+        $x("//*[@data-test-id='date']//input").sendKeys(generateDate(daysGap, newDatePattern));
         $x("//*[@data-test-id='name']//input").setValue("Александр Бесстужев-Никольский");
         $x("//*[@data-test-id='phone']//input").setValue("+79624569137");
         $x("//*[@data-test-id='agreement']").click();
         $x("//div[contains (@class, 'grid-row')]//button").click();
         $x("//div[contains (text(), 'Успешно!')]").shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $x("//*[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(daysGap, newDatePattern)), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void shouldTestComplexDeliveryCard() {
-        int month = date.getMonthValue();
-        LocalDate dateNew = date.plusDays(7);
-        long milliseconds = dateNew.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
-        String strMilliseconds = Long.toString(milliseconds);
+        long daysGap = 7;
+        String newDatePattern = "dd.MM.yyyy";
 
         $x("//*[@data-test-id='city']//input").setValue("См");
         $x("//*[contains(text(),'Смоленск')]").click();
         $x("//*[@data-test-id='date']//button").click();
-        if (month != dateNew.getMonthValue()) {
+        if (LocalDate.now().getMonthValue() != LocalDate.now().plusDays(daysGap).getMonthValue()) {
             $x("//*[(@data-step='1')]").click();
         }
-        $x("//*[@data-day='" + strMilliseconds + "']").click();
+        $x("//*[@data-day='" + generateMilliseconds(daysGap) + "']").click();
         $x("//*[@data-test-id='name']//input").setValue("Иван Петров-Разумовский");
         $x("//*[@data-test-id='phone']//input").setValue("+79628429586");
         $x("//*[@data-test-id='agreement']").click();
         $x("//div[contains (@class, 'grid-row')]//button").click();
         $x("//div[contains (text(), 'Успешно!')]").shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $x("//*[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(daysGap, newDatePattern)), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    public long generateMilliseconds(long addDays) {
+        return LocalDate.now().plusDays(addDays).atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
+    }
 }
